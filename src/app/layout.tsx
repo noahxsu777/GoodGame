cd865041f34ef72849de3d6efd2f0312579d9f02
noahxsu777 <noahxdc12@gmail.com>
2026-09-01 22:25:16 -0400

    design: importar liquid glass

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./motion.css";
+import "./liquid.css";
 import { SiteHeader } from "@/components/site-header";
 import { SiteFooter } from "@/components/site-footer";
 import { currentUser } from "@/lib/session";
